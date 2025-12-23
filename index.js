dcf63a62564400959f6223350889ab0415be159e
Developerr18 <dineshnikum1@gmail.com>
2025-12-23 17:15:18 +0530

    ref

--- index.js
+++ index.js
@@ -5,3 +5,17 @@ console.log("it is a logout branch");
 console.log("the logout feature");
 console.log("logout feature completed");
 console.log("sidebar feature");
+
+/*
+git init                    # Initialize repository
+git clone <url>             # Clone repository
+git status                  # Check status
+git add <file>              # Stage changes
+git commit -m "message"     # Commit changes
+git push origin <branch>    # Push to remote
+git pull origin <branch>    # Pull from remote
+git branch                  # List branches
+git checkout -b <branch>    # Create and switch branch
+git merge <branch>          # Merge branch
+git log                     # View history
+*/
